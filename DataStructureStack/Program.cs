0a99dadb25763e54081baca1161226211c47699a
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-20 14:52:00 +0530

    Queue

--- DataStructureStack/Program.cs
+++ DataStructureStack/Program.cs
@@ -7,19 +7,29 @@ namespace StacksAndQueues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Stack and Queues");
-            Console.WriteLine("Stack Program");
-            Stacks stacks = new Stacks();
-            stacks.Push(70);
-            stacks.Push(30);
-            stacks.Push(56);
-            stacks.Display();
+            //Console.WriteLine("Welcome To Stack and Queues");
+            //Console.WriteLine("Stack Program");
+            //Stacks stacks = new Stacks();
+            //stacks.Push(70);
+            //stacks.Push(30);
+            //stacks.Push(56);
+            //stacks.Display();
 
-            stacks.Peek();
+            //stacks.Peek();
 
-            stacks.Pop();
+            //stacks.Pop();
+
+            //stacks.IsEmpty();
+
+            Console.WriteLine("welcome to Queue");
             
-            stacks.IsEmpty();
+            Queue queue = new Queue();
+            
+            queue.Enqueue(44);
+            queue.Enqueue(33);
+            queue.Enqueue(23);
+            queue.DisplayQueue();
+
         }
     }
 }
---
+++ DataStructureStack/Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Queue
+    {
+        
+        public Node head;
+        
+        public void Enqueue(int data)
+        {
+            
+            Node node = new Node(data);
+            Node temp = head;
+            
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+              
+                Node lastNode = GetLastNode(temp);
+                lastNode.next = node;
+            }
+            Console.WriteLine("Element inserted in queue:\t{0}", node.data);
+
+        }
+        
+        public Node GetLastNode(Node temp)
+        {
+            if (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+       
+        public void DisplayQueue()
+        {
+            Console.WriteLine("\nDisplaying the Queue");
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine("empty queue");
+            }
+            else
+            {
+                
+                
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
